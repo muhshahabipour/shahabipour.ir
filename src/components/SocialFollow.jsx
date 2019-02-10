d33e6ed66d6c787741c2f82da441b8ec2412cec9
muh.shahabipour <shahabi@>
2019-02-11 01:51:13 +0330

    add social links

--- src/components/SocialFollow.jsx
+++ src/components/SocialFollow.jsx
@@ -11,22 +11,22 @@ export default class SocialFollow extends React.Component {
 
         return (
             <div className="d-flex justify-content-center justify-content-sm-start ">
-                <a href="" className=" text-dark-50">
+                <a href="https://www.linkedin.com/in/muhshahabipour/">
                     <i className="fab fa-linkedin fa-2x mr-3" style={{color: "#0077b5"}}></i>
                 </a>
-                <a href="" className=" text-dark-50">
+                <a href="javascript:void(0)">
                     <i className="fab fa-skype fa-2x mr-3" style={{color: "#00aff0"}}></i>
                 </a>
-                <a href="" className=" text-dark-50">
+                <a href="https://github.com/muhshahabipour">
                     <i className="fab fa-github-square fa-2x mr-3" style={{color: "#333"}}></i>
                 </a>
-                <a href="" className=" text-dark-50">
+                <a href="https://github.com/muhshahabipour">
                     <i className="fab fa-codepen fa-2x mr-3" style={{color: "#0ebeff"}}></i>
                 </a>
-                <a href="" className=" text-dark-50">
+                <a href="https://twitter.com/muhshahabipour">
                     <i className="fab fa-twitter-square fa-2x mr-3" style={{color: "#1da1f2"}}></i>
                 </a>
-                <a href="" className=" text-dark-50">
+                <a href="https://www.instagram.com/muhshahabipour/">
                     <i className="fab fa-instagram fa-2x mr-3 instagram-font-color"></i>
                 </a>
             </div>
